c50f15f7c2b7e4740a40397cc07419530733971e
cecil <cecilie.wang@hotmail.com>
2018-05-04 09:03:50 +0200

    database og forside

---
+++ app/src/main/java/com/example/cecil/database2/StartActivity.java
@@ -0,0 +1,14 @@
+package com.example.cecil.database2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.cecil.database2.R;
+
+public class StartActivity extends AppCompatActivity {
+
+}
